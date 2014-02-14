c8055153e32c9a21725108d4114b20d2437b8efe
muymoo <matt.momont@gmail.com>
2014-02-14 00:08:01 -0800

    Removed unused code. Added TODOs. Added XML parsing example.

--- app/services/MechanicalTurkService.java
+++ app/services/MechanicalTurkService.java
@@ -15,11 +15,11 @@ import java.util.Map;
 
 import play.Logger;
 
+import com.amazonaws.mturk.dataschema.QuestionFormAnswers;
 import com.amazonaws.mturk.requester.Assignment;
 import com.amazonaws.mturk.requester.AssignmentStatus;
 import com.amazonaws.mturk.requester.HIT;
 import com.amazonaws.mturk.service.axis.RequesterService;
-import com.amazonaws.mturk.service.exception.ServiceException;
 import com.amazonaws.mturk.util.PropertiesClientConfig;
 
 /**
@@ -28,61 +28,14 @@ import com.amazonaws.mturk.util.PropertiesClientConfig;
 public class MechanicalTurkService
 {
     private RequesterService service;
-
-    // Defining the attributes of the HIT to be created
-    private String           title          = "Answer a question";
-    private String           description    = "This is a HIT created by the Mechanical Turk SDK.  Please answer the provided question.";
-    private int              numAssignments = 1;
-    private double           reward         = 0.05;
+    private double           reward = 0.05;
 
     /**
-     * Constructor
-     * 
+     * Initialize the Mechanical Turk service
      */
     public MechanicalTurkService()
     {
         service = new RequesterService(new PropertiesClientConfig("conf/mturk.properties"));
-    }
-
-    /**
-     * Check if there are enough funds in your account in order to create the HIT
-     * on Mechanical Turk
-     * 
-     * @return true if there are sufficient funds. False if not.
-     */
-    public boolean hasEnoughFund()
-    {
-        double balance = service.getAccountBalance();
-        System.out.println("Got account balance: " + RequesterService.formatCurrency(balance));
-        return balance > reward;
-    }
-
-    /**
-     * Creates the simple HIT.
-     * 
-     */
-    public void createHelloWorld()
-    {
-        try
-        {
-
-            // The createHIT method is called using a convenience static method of
-            // RequesterService.getBasicFreeTextQuestion that generates the QAP for
-            // the HIT.
-            HIT hit = service.createHIT(title, description, reward,
-                    RequesterService.getBasicFreeTextQuestion("What is the weather like right now in Seattle, WA?"),
-                    numAssignments);
-
-            System.out.println("Created HIT: " + hit.getHITId());
-
-            System.out.println("You may see your HIT with HITTypeId '" + hit.getHITTypeId() + "' here: ");
-            System.out.println(service.getWebsiteURL() + "/mturk/preview?groupId=" + hit.getHITTypeId());
-
-        }
-        catch (ServiceException e)
-        {
-            System.err.println(e.getLocalizedMessage());
-        }
     }
 
     /**
@@ -106,12 +59,17 @@ public class MechanicalTurkService
 
         Logger.debug("View HIT here: " + service.getWebsiteURL() + "/mturk/preview?groupId=" + hit.getHITTypeId());
 
+        // TODO: I'm not sure if this is the right ID to return to find the hit again. It doesn't seem to work. There is a different ID on the actual HIT page
+        // that is used to retrieve the assignments. Could be .getHITGroupId() ??
         return hit.getHITId();
     }
 
     /**
+     * Ex.
+     * {"lovely":15, "happy":2, "nice": 3, ...}
+     * 
      * @param hitId
-     * @return
+     * @return Frequency map of words and their counts
      */
     public Map<String, Integer> getWordsFromCompletedAssignments(String hitId)
     {
@@ -130,13 +88,18 @@ public class MechanicalTurkService
     private Map<String, Integer> parseAssignmentsForWords(Assignment[] assignments)
     {
         Map<String, Integer> wordCounts = new HashMap<String, Integer>();
-        
+
         for (Assignment assigment : assignments)
         {
             if ( assigment.getAssignmentStatus() == AssignmentStatus.Submitted )
             {
+                // TODO: This is an XML string
                 String word = assigment.getAnswer();
-
+                
+                // TODO: This is some parsing code that should get you close. See the Reviewer sample for more info.
+                // QuestionFormAnswers questionFormAnswers = RequesterService.parseAnswers(word);
+                // questionFormAnswers.getAnswer();
+                
                 // Increment word count
                 int count = wordCounts.containsKey(word) ? wordCounts.get(word) : 0;
                 wordCounts.put(word, count);
